1dd20a8531396c5cb9140eb25aa8a86eb5b2f14f
Ricardo Camacho <rscamachodev@gmail.com>
2021-12-29 19:48:08 -0300

    SOLID LSP in CategoriesRepository

---
+++ src/repositories/PostgressCategoryRepository.ts
@@ -0,0 +1,20 @@
+import { Category } from '../model/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from './ICategoriesRepository';
+
+class PostgressCategoryRepository implements ICategoriesRepository {
+  create({ name, description }: ICreateCategoryDTO): void {
+    console.log(name, description);
+  }
+  list(): Category[] {
+    return null;
+  }
+  findByName(name: string): Category {
+    console.log(name);
+    return null;
+  }
+}
+
+export { PostgressCategoryRepository };
