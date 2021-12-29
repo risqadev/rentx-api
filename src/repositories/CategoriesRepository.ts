b255e17c3c6c718166daff4dd94aefef72b9d97d
Ricardo Camacho <rscamachodev@gmail.com>
2021-12-29 14:00:32 -0300

    validating and listing categories

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -16,6 +16,15 @@ class CategoriesRepository {
     const category = new Category(name, description);
     this.categories.push(category);
   }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find(category => category.name === name);
+    return category;
+  }
 }
 
 export { CategoriesRepository };
